fc44af1332b6289779091b3b503853e4a27a27ec
gaughanln <gaughanln@yahoo.com>
2023-03-01 10:25:17 -0600

    added  data to the routes files

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: // ???
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+//what goes here?
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: category_id, //can't find this
+  through:{
+    model:ProductTag,
+    unique: true,
+  },
+  as: 'table_name' //rename this
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: category_id,
+  through:{
+    model:ProductTag, //is  this right?
+    unique: true,
+  },
+  as: 'product_tags' //rename this
+});
 
 module.exports = {
   Product,
